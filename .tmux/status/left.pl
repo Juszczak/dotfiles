2e751e888444c0b074b029bfb8c9f6538ed08c0b
Adrian Juszczak <adrian@juszczak.io>
2017-10-06 23:44:20 +0200

    Tmux: Environment for Vim
    - truecolor and hex colors
    - italics font
    - pasteboard
    - escape time
    
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- .tmux/status/left.pl
+++ .tmux/status/left.pl
@@ -7,7 +7,7 @@ $seg_2_color = "#[fg=$TMUX_THEME_COLOR_FOURTH,bg=$TMUX_THEME_COLOR_THIRD]";
 $seg_3_color = "#[fg=$TMUX_THEME_COLOR_THIRD,bg=$TMUX_THEME_COLOR_FIFTH]";
 
 $seg_1_content = "#S";
-$seg_2_content = "#{?client_prefix,#[fg=$TMUX_THEME_COLOR_SIXTH]•,#[fg=$TMUX_THEME_COLOR_FIFTH]•}";
+$seg_2_content = "#{?client_prefix,#[fg=#eeeeee]•,#[fg=#242424]•}";
 $seg_3_content = "";
 
 $status_left = "$seg_1_color $seg_1_content $seg_2_color $seg_2_content $seg_3_color $seg_3_content ";
