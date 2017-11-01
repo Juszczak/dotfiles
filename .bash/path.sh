9caaa2ac33442897a4f016f51d4ae1ed7c7f722a
Adrian Juszczak <adrian@juszczak.io>
2017-11-01 18:15:00 +0100

    dots: dotfiles install and management scripts
    - dots updated to work from PATH
    - install script
    - PATH updated: dots, cargo, pub, flutter
    
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- .bash/path.sh
+++ .bash/path.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
+# Custom PATH
+export PATH="$HOME/.bin:$PATH"
+
+# Python
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
+# Rust/Cargo
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# Flutter
+export PATH="/usr/local/opt/flutter/sdk/bin:$PATH"
+
+# Dart pub cache
+export PATH="$HOME/.pub-cache/bin:$PATH"
